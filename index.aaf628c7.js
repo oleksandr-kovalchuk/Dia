9c15459bd628d23022e95b39e7e19da1e8ca5398
Oleksandr Kovalchuk <kov280396@gmail.com>
2025-06-24 10:52:29 +0300

    make build

---
+++ index.aaf628c7.js
@@ -0,0 +1,2 @@
+for(var e=document.querySelectorAll(".slider__btn"),t=0;t<e.length;t++)e[t].addEventListener("click",function(){alert("The feature is still under development")});
+//# sourceMappingURL=index.aaf628c7.js.map
